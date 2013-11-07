1f207b253280c8c32c9ae1ac87d6d4bd10da512d
Roll <Roll>
2013-11-08 01:55:02 +0200

    delete users

--- server/setup.js
+++ server/setup.js
@@ -4,5 +4,6 @@ Meteor.startup(function(){
   }
   Posts.allow({ insert: thisTime});
   Messages.allow({ insert: thisTime});
-});
+  Meteor.users.remove({username: {$in: ["shiva3.artist@gmail.com", "Лох", "Testusername"]}});
+ });
 
